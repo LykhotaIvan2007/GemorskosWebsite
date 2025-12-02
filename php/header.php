0dcb67cae5b000909b11108133bddf5fbb58e185
LykhotaIvan <ivan.lykhota@student.nhlstenden.com>
2025-12-02 22:21:37 +0100

    created folders for php,css,images,fonts and header

---
+++ php/header.php
@@ -0,0 +1,12 @@
+<header>
+        <img class="gemorskosLogo" src="../images/gemorskosLogo.png" alt="logo">
+        <ul class="headerLinksUl">
+            <li><a class="headerLink" href="">Latest news</a></li>
+            <li><a class="headerLink"  href="">Top news</a></li>
+            <li><a class="headerLink" href="">News of all time</a></li>
+        </ul>
+        <ul class="foremployeesDiv">
+          <li><a class="employeesLink" href="">For employees</a></li>  
+          <img class="personImg" src="../images/personImage.png" alt="person">
+        </ul>
+</header>
